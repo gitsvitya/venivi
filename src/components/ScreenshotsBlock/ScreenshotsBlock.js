ac204b9bbfbbf35b1a3615aa5dde75a1d400bba7
Виктор Строков <strokov.victor@gmail.com>
2023-08-17 22:25:35 +0300

    Added: General Modal functionality (without content)

--- src/components/ScreenshotsBlock/ScreenshotsBlock.js
+++ src/components/ScreenshotsBlock/ScreenshotsBlock.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import styles from './ScreenshotsBlock.module.css';
+import Modal from '../Modal/Modal';
 
 function ScreenshotsBlock() {
 
+  const [ModalOpen, ModalOpened] = React.useState(false);
+
+  function openModal() {
+    ModalOpened(true);
+  };
+
+  function closeModal() {
+    ModalOpened(false);
+  };
+
   return (
+    <>
     <div className={styles.screenshotsBlock}>
       <div className={styles.container}>
         <h2 className={styles.header}>How it looked like?</h2>
         <div className={styles.screenshotsBox}>
-          <figure className={styles.screenshotBox}>
+          <figure className={styles.screenshotBox} onClick={openModal}>
             <div className={styles.screenshotMain}></div>
             <figcaption className={styles.screenshotFig}>Main page</figcaption>
           </figure>
-          <figure className={styles.screenshotBox}>
+          <figure className={styles.screenshotBox} onClick={openModal}>
             <div className={styles.screenshotCont}></div>
             <figcaption className={styles.screenshotFig}>Contest screen</figcaption>
           </figure>
         </div>
       </div>
     </div>
+    {
+      ModalOpen &&
+      <Modal closeModal={closeModal}>
+        <div className={styles.data}></div> #TODO
+      </Modal>
+    }
+    </>
   );
 }
 
