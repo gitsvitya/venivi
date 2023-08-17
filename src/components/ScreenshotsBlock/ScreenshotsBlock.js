f57bf030eb1e3198c3786e1a2faf0835ecca5e13
Виктор Строков <strokov.victor@gmail.com>
2023-08-18 02:41:06 +0300

    Added: modal window functionality

--- src/components/ScreenshotsBlock/ScreenshotsBlock.js
+++ src/components/ScreenshotsBlock/ScreenshotsBlock.js
@@ -5,6 +5,7 @@ import Modal from '../Modal/Modal';
 function ScreenshotsBlock() {
 
   const [ModalOpen, ModalOpened] = React.useState(false);
+  const [ModalContent, SetModalContent] = React.useState('');
 
   function openModal() {
     ModalOpened(true);
@@ -14,17 +15,29 @@ function ScreenshotsBlock() {
     ModalOpened(false);
   };
 
+  function renderModalContent(input) {
+
+    if (input==='screenshotMain')
+      return (
+        <div className={styles.screenshotMain}></div>
+      );
+    else
+      return (
+        <div className={styles.screenshotCont}></div>
+      );
+  };
+
   return (
     <>
     <div className={styles.screenshotsBlock}>
       <div className={styles.container}>
         <h2 className={styles.header}>How it looked like?</h2>
         <div className={styles.screenshotsBox}>
-          <figure className={styles.screenshotBox} onClick={openModal}>
+          <figure className={styles.screenshotBox} onClick={()=>{openModal(); SetModalContent('screenshotMain')}}>
             <div className={styles.screenshotMain}></div>
             <figcaption className={styles.screenshotFig}>Main page</figcaption>
           </figure>
-          <figure className={styles.screenshotBox} onClick={openModal}>
+          <figure className={styles.screenshotBox} onClick={()=>{openModal(); SetModalContent('screenshotCont')}}>
             <div className={styles.screenshotCont}></div>
             <figcaption className={styles.screenshotFig}>Contest screen</figcaption>
           </figure>
@@ -34,7 +47,7 @@ function ScreenshotsBlock() {
     {
       ModalOpen &&
       <Modal closeModal={closeModal}>
-        <div className={styles.data}></div> #TODO
+        `${renderModalContent(ModalContent)}`
       </Modal>
     }
     </>
